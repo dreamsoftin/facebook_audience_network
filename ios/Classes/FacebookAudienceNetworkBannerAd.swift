bf8b775108355de3e416abe8326fc0c87da77c0a
Matt Hilleman <matt@kodiakcodeworks.com>
2021-10-08 00:17:48 -0500

    WIP: Trying to see if we can use the new sizes

--- ios/Classes/FacebookAudienceNetworkBannerAd.swift
+++ ios/Classes/FacebookAudienceNetworkBannerAd.swift
@@ -129,11 +129,22 @@ class FacebookAudienceNetworkBannerAdView: NSObject, FlutterPlatformView, FBAdVi
             ? true : false
         let valueId: String = existsId
             ? self.params["id"] as! String : ""
-        let width: CGFloat = (self.params["width"] != nil)
-            ? self.params["width"] as! CGFloat : 320.0
         let height: CGFloat = (self.params["height"] != nil)
             ? self.params["height"] as! CGFloat : 50.0
-        let adSize: FBAdSize = FBAdSize.init(size: CGSize.init(width: width, height: height))
+		var adSize
+		if (height >= 250.0)
+		{
+			adSize = kFBAdSizeHeight250Rectangle
+		}
+		else if (height >= 90.0)
+		{
+			adSize = kFBAdSizeHeight90Banner
+		}
+		else 
+		{
+			adSize = kFBAdSizeHeight50Banner
+		}
+        //let adSize: FBAdSize = FBAdSize.init(size: CGSize.init(width: width, height: height))
         
         if (existsId) {
             self.bannerAd = FBAdView.init(placementID: valueId, adSize: adSize, rootViewController: UIApplication.shared.keyWindow?.rootViewController)
